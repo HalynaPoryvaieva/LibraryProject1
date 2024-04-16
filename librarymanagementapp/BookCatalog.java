8f50cae2aaa5fd4e2a0715901009328dfd271ad6
Ilofrudd <boyko5000@gmail.com>
2024-04-16 18:56:25 +0200

    Update BookCatalog.java

--- librarymanagementapp/BookCatalog.java
+++ librarymanagementapp/BookCatalog.java
@@ -1,4 +1,31 @@
 package librarymanagementapp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BookCatalog {
+    private Map<Integer, Book> books;
+
+    public BookCatalog(Map<Integer, Book> books) {
+        this.books = new HashMap<>(books);
+    }
+    public void addBook(Book book) {
+        books.put(book.getCatalogNumber(), book);
+    }
+    public boolean removeBook(int catalogNumber) {
+        if (books.containsKey(catalogNumber)) {
+            books.remove(catalogNumber);
+            return true;
+        }
+        return false;
+    }
+    public Map<Integer, Book> findBook(String searchQuery, String type) {
+
+    }
+    public void sortBooks(String sortBy) {
+
+    }
+    public void printCatalog() {
+        books.values().forEach(System.out::println);
+    }
 }
