2c094be23134e337d9b5b34204b03c9c970f6dc7
Larysa Petrova <158000446+larysa7408@users.noreply.github.com>
2024-04-17 12:50:52 +0200

    Commit

---
+++ librarymanagementapp/LibraryCard.java
@@ -0,0 +1,58 @@
+package librarymanagementapp;
+/**
+ * AIT-TR, cohort 42.1, Java Basic, Project1
+ *
+ * @author Larysa Petrova
+ * @version 17-Apr-24
+ */
+
+import librarymanagementapp.BookCatalog;
+import librarymanagementapp.User;
+import librarymanagementapp.UserCard;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LibraryCard {
+    private Map<Integer, UserCard> userCards;
+
+    public LibraryCard() {
+        BookCatalog bookCatalog = new BookCatalog();
+        userCards = new HashMap<>();
+    }
+
+    /**
+     * @param user
+     * @param limit
+     */
+    public void addNewUserCard(User user, int limit) {
+        if (!userCards.containsKey(user.getUserId())) {
+            UserCard userCard = new UserCard(user.getName(), user.getUserId(), limit);
+            userCard.put(user.getUserId(), userCard);
+            System.out.println("User card created successfully for user: " + user.getName());
+        } else {
+            System.out.println("User card already exists for user: " + user.getName());
+        }
+    }
+
+    public boolean closeUserCard(int userId) {
+        if (userCards.containsKey(userId)) {
+            userCards.remove(userId);
+            System.out.println("User card closed successfully for user ID: " + userId);
+            return true;
+        } else {
+            System.out.println("User card not found for user ID: + userId");
+            return false;
+        }
+    }
+
+    public void reopenUserCard(int userId) {
+        if (userCards.containsKey(userId)) {
+            UserCard userCard = userCards.get(userId);
+            userCard.setClosed(false);
+            System.out.println("User card reopened successfully for user ID: " + userId);
+        } else {
+            System.out.println("User card not found for user ID: " + userId);
+        }
+    }
+}
--- librarymanagementapp/UserCard.java
+++ librarymanagementapp/UserCard.java
@@ -1,4 +1,12 @@
 package librarymanagementapp;
 
 public class UserCard {
+    public UserCard(String name, Integer userId, int limit) {
+    }
+
+    public void put(Integer userId, UserCard userCard) {
+    }
+
+    public void setClosed(boolean b) {
+    }
 }
